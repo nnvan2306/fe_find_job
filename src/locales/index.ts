6ab812d71c303d02a82a4320d48625042e2abae8
Ngô Ngọc Văn <vanvanvan23062003@gmail.com>
2025-05-23 22:39:46 +0700

    up

--- src/locales/index.ts
+++ src/locales/index.ts
@@ -2,14 +2,14 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 
-import indexEn from "./en/index.json";
+// import indexEn from "./en/index.json";
 import indexVi from "./vi/index.json";
 
 i18n.use(LanguageDetector)
     .use(initReactI18next)
     .init({
         resources: {
-            en: { translation: indexEn },
+            // en: { translation: indexEn },
             vi: { translation: indexVi },
         },
         fallbackLng: "vi",
